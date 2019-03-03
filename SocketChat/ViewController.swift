08d0abb463fd5b899850feccd9fa875579dcee12
Kiryl <Kiryl.co@gmail.com>
2019-03-03 22:07:09 +0300

    Initial Commit

---
+++ SocketChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocketChat
+//
+//  Created by Kiryl Holubeu on 3/3/19.
+//  Copyright © 2019 brakhmen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
